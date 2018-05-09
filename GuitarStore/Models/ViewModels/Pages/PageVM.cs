641898e80a3787f2582235fd97bd2629832835f6
User <User@PB-LAPTOP>
2018-05-09 19:33:22 +0100

    Added ckeditor & fileman

--- GuitarStore/Models/ViewModels/Pages/PageVM.cs
+++ GuitarStore/Models/ViewModels/Pages/PageVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace GuitarStore.Models.ViewModels.Pages
 {
@@ -23,10 +24,10 @@ namespace GuitarStore.Models.ViewModels.Pages
         }
 
         public int Id { get; set; }
-        [Required, StringLength(50, MinimumLength =3)]
+        [Required, StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
         public string Slug { get; set; }
-        [Required, StringLength(int.MaxValue, MinimumLength = 3)]
+        [Required, StringLength(int.MaxValue, MinimumLength = 3), AllowHtml]
         public string Body { get; set; }
         public int Sorting { get; set; }
     }
